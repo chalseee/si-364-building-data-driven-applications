f674c2acea9444855a302f79be3233e1a38efd80
chalseoko <chalseoko@gmail.com>
2018-04-11 00:30:03 -0400

    updated app plan

--- HW6/SI364final.py
+++ HW6/SI364final.py
@@ -7,6 +7,9 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 
+from flask_login import LoginManager, login_required, logout_user, login_user, UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 app.debug = True
 app.use_reloader = True
@@ -21,6 +24,107 @@ manager=Manager(app)
 db=SQLAlchemy(app)
 migrate=Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+#Models
+#includes 1-many relationship between words and definitions
+#includes many-many relationship betweeen words and partofspeech
+pos_word = db.Table('pos_word', db.Column('pos_id', db.Integer, db.ForeignKey('partofspeech.id')), db.Column('word_id', db.Integer, db.ForeignKey('words.id')))
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Word(db.Model):
+    __tablename__ = "words"
+    id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String(5))
+    word = db.Column(db.String(32))
+    phonetic_spelling = db.Column(db.String(32))
+
+class Definition(db.Model):
+    __tablename__ = "definitions"
+    id = db.Column(db.Integer, primary_key=True)
+    definition = db.Column(db.String(264))
+    example_sentence = db.Column(db.String(264))
+    word_id = db.relationship('Word', backref="Definition")
+
+class PartOfSpeech(db.Model):
+    __tablename__ = "partofspeech"
+    id = db.Column(db.Integer, primary_key=True)
+    part_of_speech = db.Column(db.String(32))
+
+#Form classes will go here
+
+
+#View functions - all include the base navigation menu
+@app.errorhandler(404)
+def page_not_found(e):
+    pass #error-handler for a 404 error renders 404.html
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    pass #error-handler for a 500 error. renders 500.html
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    pass #should render a page with a form allowing users to login
+
+@app.route('/logout')
+@login_required
+def logout():
+    pass #should render a page with a form allowing users to logout
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+    pass #should render a page with a form allowing users to make an account
+
+@app.route('/secret')
+@login_required
+def secret():
+    pass #should render if a user goes on a page that they have to be logged in to use.
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    pass #homepage. should render a login form as well as a menu to view all other pages in app.
+
+@app.route('/all_users_words')
+def all_words(): #should render a page that shows information about all words that have been added on this app.
+    pass #doesnt require login.
+
+@login_required
+@app.route('/delete/<word_id>')
+def delete(): #should allow the user to delete a word from their list of words.
+    pass #requires login
+
+@login_required
+@app.route('/update/<word_id>')
+def update(): #should allow the user to change a word from their list of words and then run a get_or_create fxn to update possible new parts of speech or definitions
+    pass #requires login
+
+@login_required
+@app.route('/your_words')
+def your_words(): #displays all words that the user has added to their collection. also displays a form to enter a word. page will update to show this word in addition to all past ones once the form is submitted.
+    pass #requires login
+
 
 
 if __name__ == "__main__":
--- HW6/practice_api.py
+++ HW6/practice_api.py
@@ -2,8 +2,12 @@ import requests, json
 
 app_id = "be77c0cb"
 app_key = "bf29b57815ffe51f465ca7069db9135c"
-base_url="https://od-api.oxforddictionaries.com/api/v1/entries/en/programming"
+base_url="https://od-api.oxforddictionaries.com/api/v1/entries/en/"
+word_id=str(input("enter word to search: \n"))
 
-data = requests.get(base_url, headers={'app_id': app_id, 'app_key':app_key}).json()
+def oxford_dict_request(app_id, app_key, base_url, word_id):
+    base_url = base_url + word_id.lower()
+    data = requests.get(base_url, headers={'app_id': app_id, 'app_key':app_key})
+    return data.json()['results']
 
-print(data)
+print(oxford_dict_request(app_id, app_key, base_url, word_id))
